4577fb0ac0bed457c8355809c2f4a628c0f0c010
Tamal White <tamalw@me.com>
2011-11-15 00:21:56 -0600

    Initial pass on luhnybin
    Require Ruby 1.9.2

---
+++ init.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib/luhnybin'
+
+$stdin.each do |test|
+  filtered_text = Luhnybin.filter(test)
+	begin
+    puts filtered_text
+    $stdout.flush
+  rescue Errno::EPIPE
+    break
+  end
+end
---
+++ lib/luhnybin.rb
@@ -0,0 +1,33 @@
+require_relative 'luhn'
+
+class Luhnybin
+
+	def	self.filter(input)
+		@text = input
+		
+		check_for_possible_matches
+		filter_text
+		
+		@text
+	end
+	
+	def	self.check_for_possible_matches
+		@matches = []
+		0.upto(@text.length) do	|pos|
+			12.upto(17) do |length|
+				m = /\d[\d\s-]{#{length}}\d/.match(@text, pos)
+				next unless (14..16).include?(m.to_s.scan(/\d/).size)
+				next unless m.to_s.scan(/\d/).join.to_i.valid_luhn?
+				
+				@matches << m.offset(0) unless m.nil?
+			end
+		end
+	end
+	
+	def self.filter_text
+		@matches.each do |m|
+			@text[m[0], m[1]-m[0]] = @text[m[0], m[1]-m[0]].gsub(/\d/, 'X')
+		end
+	end
+
+end
